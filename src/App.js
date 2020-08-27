1d44f94539347513be27d1a13d9f34bf8c59ced5
Alexander <Alexander.risteski23@gmail.com>
2020-08-27 18:38:02 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,70 @@ import React from "react";
 import Filter from './Components/Filter'
 import Content from './Components/Content'
 import styled from "styled-components";
+import icon from "./assets/Vector.png"
+import icon2 from "./assets/VectorB.png"
 const Main = styled.div`
   text-align: center;
 `
 const Bar = styled.div`
   height: 50px;
-  background-color: #3E6A8D;
+  display: flex;
+    justify-content: space-between;
+  background-color: #144063;
+  .image{
+    background-image: url(${icon});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 50px;
+    width: 180px;
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    cursor: pointer;
+  }
+  button{
+    margin-top: 10px;
+    height: 30px;
+    background: transparent;
+    border: none;
+    margin-right: 10px;
+    color: white;
+    font-size: 15px;
+    outline: none;
+    margin-left: 23px;
+  }
+  button:hover{
+    text-decoration-line: underline;
+ 
+  }
+  h2{
+    color: white;
+    margin-top: 8px;
+    text-decoration-line: underline;
+  text-decoration-style: wavy;
+  margin-right: 25px;
+  }
+
+  
+`
+const Reflection = styled.div`
+  background-image: url(${icon2});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 50px;
+    width: 180px;
+    transform: rotateX(-180deg);   
 `
 function App() {
   return <Main>
-    <Bar>Hello</Bar>
+    <Bar>
+      <div className="image"></div>
+      <h2>BerrMarket $</h2>
+      <div><button>Add Ship</button>
+      </div>
+      </Bar>
+    <Reflection></Reflection>
 	  <Filter></Filter>
     <Content></Content>
   </Main>;
---
+++ src/Components/Add.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+
+function Add() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Add
